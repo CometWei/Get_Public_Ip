15b72bb6cb450c789faa4dce75b7eb7f4dda18f8
AustinComet <reveno415066@gmail.com>
2021-09-02 14:38:31 +0800

    First Push

---
+++ Get_public_ip.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/tomasen/realip"
+)
+
+func main() {
+	http.HandleFunc("/", getip)
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func getip(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	r.ParseForm()
+	clientIP := realip.FromRequest(r)
+	log.Println("GET from", clientIP)
+	fmt.Fprintln(w, clientIP)
+}
