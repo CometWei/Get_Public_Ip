82fa1777967d46468d9d5cc9f216d6e61e3856e2
AustinComet <reveno415066@gmail.com>
2021-09-03 14:18:05 +0800

    Markdown demo dependency

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 // +heroku goVersion go1.16
 
 require (
+	github.com/oschwald/geoip2-golang v1.5.0 // indirect
 	github.com/tomasen/realip v0.0.0-20180522021738-f0c99a92ddce // indirect
 	golang.org/x/net v0.0.0-20210902165921-8d991716f632 // indirect
 )
--- main.go
+++ main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
 
+	"github.com/oschwald/geoip2-golang"
 	"github.com/tomasen/realip"
 )
 
@@ -21,4 +23,22 @@ func getip(w http.ResponseWriter, r *http.Request) {
 	clientIP := realip.FromRequest(r)
 	log.Println("GET from", clientIP)
 	fmt.Fprintln(w, clientIP)
+
+	db, err := geoip2.Open("./GeoLite2-City.mmdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	// If you are using strings that may be invalid, check that ip is not nil
+	ip := net.ParseIP(clientIP)
+	record, err := db.City(ip)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Portuguese (BR) city name: %v\n", record.City.Names["zh-CN"])
+	fmt.Printf("English subdivision name: %v\n", record.Subdivisions[0].Names["en"])
+	fmt.Printf("Russian country name: %v\n", record.Country.Names["en"])
+	fmt.Printf("ISO country code: %v\n", record.Country.IsoCode)
+	fmt.Printf("Time zone: %v\n", record.Location.TimeZone)
+	fmt.Printf("Coordinates: %v, %v\n", record.Location.Latitude, record.Location.Longitude)
 }
